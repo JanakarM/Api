cc687c755612cc2d4cdfcefddd1244dd66f91f82
JanakarM <30556870+JanakarM@users.noreply.github.com>
2017-09-19 21:18:03 +0530

    success

---
+++ src/java/servlet/DBConnector.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.sql.*;
+public class DBConnector{
+
+
+    public DBConnector()
+    {
+        createDBConnection();
+    }
+  public Connection createDBConnection()
+  {
+      Connection con=null;
+            
+            ResultSet rs;
+            try
+            {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:80/db","root","1234");
+            }
+            catch(Exception e){
+            e.printStackTrace();}
+            
+  return con;
+       
+}
+}
